6587683fdce7f4cb4660855601e26364d82bd95d
manmdyalw@gmail.com <maze014.dev.prod@gmail.com>
2024-12-18 01:41:16 +0100

    restore origin

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    //origin: 'https://luniversdemm.store',
-    origin: '*',
+    origin: 'https://luniversdemm.store',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
