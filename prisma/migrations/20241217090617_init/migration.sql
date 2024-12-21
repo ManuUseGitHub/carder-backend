4f4f41975e3ea240fa0cde447b1ae18fb3ebd374
manmdyalw@gmail.com <maze014.dev.prod@gmail.com>
2024-12-21 08:17:50 +0100

    add prisma

---
+++ prisma/migrations/20241217090617_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Lov" (
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "shared" BOOLEAN NOT NULL DEFAULT false,
+    "issuer" TEXT,
+    "book" TEXT,
+
+    CONSTRAINT "Lov_pkey" PRIMARY KEY ("key")
+);
