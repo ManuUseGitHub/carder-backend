7bcc4d288506905356924cfa5296805bcdf977c2
manmdyalw@gmail.com <maze014.dev.prod@gmail.com>
2024-12-21 10:15:55 +0100

    get all lovs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,14 +2,34 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { API_VERSION_ID } from './constents';
 import { AddLOVsDTO } from './dtos/addLOVs';
+import { PrismaClient } from '@prisma/client';
+
 @Controller(API_VERSION_ID)
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
+
+  /*@Get('test')
+  getHello() {
+    return this.appService.getHello();
+  }*/
 
   @Get('test')
   getHello() {
-    return this.appService.getHello();
+    try {
+      const prisma = new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+      });
+      prisma.$on('query', (e) => {
+        console.log('Query: ' + e.query);
+        console.log('Params: ' + e.params);
+        console.log('Duration: ' + e.duration + 'ms');
+      });
+      const result = prisma.Lov.findMany();
+      console.log('done');
+      return result;
+    } catch (e) {
+      console.error(e);
+      return e;
+    }
   }
 }
